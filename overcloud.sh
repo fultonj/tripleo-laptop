7f9a9a0616cf29120088af5c6a1d598fe44da5a0
John Fulton <fulton@redhat.com>
2018-06-08 00:48:57 +0200

    modify overcloud.sh to set default root and install python-heat-client

--- overcloud.sh
+++ overcloud.sh
@@ -1,25 +1,46 @@
 #!/usr/bin/env bash
 # configure potential overcloud nodes to work with deployed-servers
-for OVER in $(grep overcloud /etc/hosts | grep -v \# | awk {'print $1'} ); do 
-    ssh $OVER -l stack "hostname" || (echo "No ssh for stack@over; exiting."; exit 1)
+
+if [[ ! -d ~/rpms ]]; then
+    mkdir ~/rpms;
+    url=https://trunk.rdoproject.org/centos7/current/
+    rpm_name=$(curl $url | grep python2-tripleo-repos | sed -e 's/<[^>]*>//g' | awk 'BEGIN { FS = ".rpm" } ; { print $1 }')
+    rpm=$rpm_name.rpm
+    curl -f $url/$rpm -o ~/rpms/$rpm
+fi
+
+for OVER in $(grep overcloud /etc/hosts | grep -v \# | awk {'print $1'} ); do
+    ssh $OVER -l root "hostname" || (echo "No ssh for root@$OVER; exiting."; exit 1)
+    ETH0_UP=$(ssh root@$OVER "ip a s eth0 | grep 192.168.24 | wc -l")
+    if [[ $ETH0_UP -eq 0 ]]; then
+	IPADDR=$(ssh root@$OVER "grep IPADDR /etc/sysconfig/network-scripts/ifcfg-eth1 | sed s/122/24/g")
+	echo $IPADDR
+	echo "Bringing up eth0 and configuring it for default route"
+	cat /dev/null > /tmp/eth0
+	echo "DEVICE=eth0" >> /tmp/eth0
+	echo "ONBOOT=yes" >> /tmp/eth0
+	echo "TYPE=Ethernet" >> /tmp/eth0
+	echo "$IPADDR" >> /tmp/eth0
+	echo "PREFIX=24" >> /tmp/eth0
+	echo "DEFROUTE=yes" >> /tmp/eth0
+	scp /tmp/eth0 root@$OVER:/etc/sysconfig/network-scripts/ifcfg-eth0
+	ssh root@$OVER "chcon system_u:object_r:net_conf_t:s0 /etc/sysconfig/network-scripts/ifcfg-eth0"
+	ssh root@$OVER "chmod 644 /etc/sysconfig/network-scripts/ifcfg-eth0"
+	ssh root@$OVER "ifup eth0"
+	ssh root@$OVER "sed -i '/DEFROUTE=yes.*/d' /etc/sysconfig/network-scripts/ifcfg-eth0"
+	echo "overcloud default route should now be be 192.168.24.1 ..."
+	ssh $OVER -l root "/sbin/ip route"
+    fi
+
     echo  "Can overcloud reach undercloud Heat API and Swift Server?"
     ssh $OVER -l stack "curl -s 192.168.24.1:8000" | jq .  # should return json
     ssh $OVER -l stack "curl -s 192.168.24.1:8080" # should 404
     echo ""
     echo "404 above is expcted ^"
     echo ""
-    echo "overcloud default route should be 192.168.24.1 ..."
-    echo ""
-    ssh $OVER -l stack "/sbin/ip route | grep default"
-    # echo "If necessary, fix with fix with..."
-    # echo "ip route add default via 192.168.24.1"
-    # ssh $OVER -l stack "echo GATEWAY='192.168.24.1' > /tmp/etc-sysconfig-network"
-    # ssh $OVER -l stack "mv /tmp/etc-sysconfig-network /etc/sysconfig/network"
-    # ssh $OVER -l stack "/sbin/ip route add default via 192.168.24.1"
-    # ssh $OVER -l stack "/sbin/ip route"
 
-    ssh $OVER -l stack "if [[ ! -d ~/rpms ]]; then mkdir ~/rpms; fi"
-    ssh $OVER -l stack "url=https://trunk.rdoproject.org/centos7/current/ ; rpm_name=$(curl $url | grep python2-tripleo-repos | sed -e 's/<[^>]*>//g' | awk 'BEGIN { FS = \".rpm\" } ; { print $1 }') ; rpm=$rpm_name.rpm ; curl -f $url/$rpm -o ~/rpms/$rpm ; sudo yum install -y ~/rpms/$rpm"
+    scp -r ~/rpms/ stack@$OVER:/home/stack/
+    ssh $OVER -l stack "sudo yum -y install rpms/*"
     ssh $OVER -l stack "sudo -E tripleo-repos current-tripleo-dev"
     ssh $OVER -l stack "sudo yum repolist"
     ssh $OVER -l stack "sudo yum -y install python-heat-agent*"
