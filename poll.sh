aff06527dca38e220eb7842ca9b89c9ecb959a59
John Fulton <fulton@redhat.com>
2018-06-08 03:10:19 +0000

    it is not necessary to have the overcloud nodes poll heat for configuration data when using config-download

--- poll.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source ~/stackrc 
-export OVERCLOUD_ROLES="Controller"
-export Controller_hosts="192.168.24.251"
-/usr/share/openstack-tripleo-heat-templates/deployed-server/scripts/get-occ-config.sh
-
-OVER=$Controller_hosts
-ssh $OVER -l stack "sudo ls -lhtr /var/lib/heat-config/heat-config-script/"
