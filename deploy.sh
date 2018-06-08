873132b3605344e73ee73e0265ce208a8b082ccb
John Fulton <fulton@redhat.com>
2018-06-08 01:20:29 +0000

    add deployed-server env files to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,11 @@ fi
 
 time openstack overcloud deploy --templates ~/templates \
      -r ~/tht/all_in_one.yaml \
-     -e ~/templates/environments/docker.yaml \
      -e ~/templates/environments/low-memory-usage.yaml \
      -e ~/templates/environments/disable-telemetry.yaml \
-     -e ~/docker_registry.yaml
+     -e ~/templates/environments/deployed-server-environment.yaml \
+     -e ~/templates/environments/docker.yaml \
+     -e ~/docker_registry.yaml \
+     -e ~/templates/environments/deployed-server-bootstrap-environment-centos.yaml \
+     -e ~/tht/ctlplane-assignments.yaml \
+     --disable-validations
