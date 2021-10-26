91ef4ded81449e8e7a29aedbe26a3203b8f43777
John Fulton <fulton@redhat.com>
2021-10-26 16:25:18 -0400

    clone directord_ceph

--- clone.sh
+++ clone.sh
@@ -105,9 +105,10 @@ fi
 
 if [[ $NAME == "undercloud" || $NAME == "standalone" || $NAME == "node0" ]]; then
     echo "ssh-keyscan github.com >> ~/.ssh/known_hosts" > git.sh
-    echo "git clone git@github.com:fultonj/tripleo-laptop.git" >> git.sh
-    echo "git clone git@github.com:fultonj/xena.git" >> git.sh
-    echo "git clone git@github.com:fultonj/task-core.git" >> git.sh
+    #echo "git clone git@github.com:fultonj/tripleo-laptop.git" >> git.sh
+    #echo "git clone git@github.com:fultonj/xena.git" >> git.sh
+    #echo "git clone git@github.com:fultonj/task-core.git" >> git.sh
+    echo "git clone git@github.com:fultonj/directord_ceph.git" >> git.sh
     scp $SSH_OPT git.sh stack@$NAME:/home/stack/
     ssh $SSH_OPT stack@$NAME "chmod 755 git.sh"
     rm git.sh
